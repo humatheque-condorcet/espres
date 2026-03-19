7106bf18eed9eca451d6fad5acebb675a4402a37
jchiavassa <jerome.chiavassa-szenberg@campus-condorcet.fr>
2026-03-19 14:16:36 +0100

    Suppression de code inutile

--- cloudapp/src/app/no-holdings/no-holdings.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-no-holdings',          
-  templateUrl: './no-holdings.component.html',
-  styleUrls: ['./no-holdings.component.scss']
-})
-export class NoHoldingsComponent {
-}
